13986cb771e48d4ef6c6f8b644ea58c9caad8a67
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-12-27 22:01:17 +0100

    refactor(expand): introduce tile group notion

--- c-sharp/TicTacToe/Board.cs
+++ c-sharp/TicTacToe/Board.cs
@@ -1,7 +1,20 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace TicTacToe; 
+namespace TicTacToe;
+
+public class TileGroup {
+    readonly List<Tile> tiles;
+
+    public bool IsFull => tiles.All(tile => tile.Symbol != ' ');
+    public bool IsTakenBySameSymbol => tiles[0].Symbol == tiles[1].Symbol &&  tiles[1].Symbol == tiles[2].Symbol;
+    
+    public TileGroup(List<Tile> tiles) {
+        this.tiles = tiles;
+    }
+    
+    public char SymbolAt(int position) => tiles[position].Symbol;
+}
 
 // Smell: Divergent change (creates the board and also operates on it)
 public class Board
