2c4945ab062750e1367931775c7067147677d860
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2025-12-27 21:37:23 +0100

    refactor: extract is row taken by same symbol variable

--- c-sharp/TicTacToe/Game.cs
+++ c-sharp/TicTacToe/Game.cs
@@ -2,34 +2,28 @@
 
 // Global Smell: Shotgun surgery around coordinates (x and y) manipulation, same for symbol.
 
-namespace TicTacToe
-{
+namespace TicTacToe {
     // Smell: Large Class.
-    public class Game
-    {
+    public class Game {
         private char _lastSymbol = ' '; // Smell: Primitive obsession.
         private Board _board = new Board();
-        
+
         // Smell: Commented code
-        public void Play(char symbol, int x, int y)
-        {
+        public void Play(char symbol, int x, int y) {
             //if first move
-            if(_lastSymbol == ' ')
-            {
+            if (_lastSymbol == ' ') {
                 //if player is X
-                if(symbol == 'O') // Smell: Magic literal
+                if (symbol == 'O') // Smell: Magic literal
                 {
                     throw new Exception("Invalid first player");
                 }
-            } 
+            }
             //if not first move but player repeated
-            else if (symbol == _lastSymbol)
-            {
+            else if (symbol == _lastSymbol) {
                 throw new Exception("Invalid next player");
             }
             //if not first move but play on an already played tile
-            else if (_board.TileAt(x, y).Symbol != ' ')
-            {
+            else if (_board.TileAt(x, y).Symbol != ' ') {
                 throw new Exception("Invalid position");
             }
 
@@ -45,52 +39,47 @@ namespace TicTacToe
         // Smell: Message chain
         // Smell: Long Method
         // Smell: Duplicated code
-        public char Winner()
-        {   //if the positions in first row are taken
+        public char Winner() {
+            //if the positions in first row are taken
             var areFirstRowPositionsTaken = _board.TileAt(0, 0).Symbol != ' ' &&
-                    _board.TileAt(0, 1).Symbol != ' ' &&
-                    _board.TileAt(0, 2).Symbol != ' ';
-            if(areFirstRowPositionsTaken)
-               {
-                    //if first row is full with same symbol
-                    if (_board.TileAt(0, 0).Symbol == 
-                        _board.TileAt(0, 1).Symbol &&
-                        _board.TileAt(0, 2).Symbol == 
-                        _board.TileAt(0, 1).Symbol )
-                        {
-                            return _board.TileAt(0, 0).Symbol;
-                        }
-               }
-                
-             //if the positions in first row are taken
-             if(_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(1, 0).Symbol == 
-                        _board.TileAt(1, 1).Symbol &&
-                        _board.TileAt(1, 2).Symbol == 
-                        _board.TileAt(1, 1).Symbol)
-                        {
-                            return _board.TileAt(1, 0).Symbol;
-                        }
+                                            _board.TileAt(0, 1).Symbol != ' ' &&
+                                            _board.TileAt(0, 2).Symbol != ' ';
+            if (areFirstRowPositionsTaken) {
+                //if first row is full with same symbol
+                var isRowTakenBySameSymbol = _board.TileAt(0, 0).Symbol ==
+                                             _board.TileAt(0, 1).Symbol &&
+                                             _board.TileAt(0, 2).Symbol ==
+                                             _board.TileAt(0, 1).Symbol;
+                if (isRowTakenBySameSymbol) {
+                    return _board.TileAt(0, 0).Symbol;
                 }
+            }
 
             //if the positions in first row are taken
-             if(_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(2, 0).Symbol == 
-                        _board.TileAt(2, 1).Symbol &&
-                        _board.TileAt(2, 2).Symbol == 
-                        _board.TileAt(2, 1).Symbol)
-                        {
-                            return _board.TileAt(2, 0).Symbol;
-                        }
+            if (_board.TileAt(1, 0).Symbol != ' ' &&
+                _board.TileAt(1, 1).Symbol != ' ' &&
+                _board.TileAt(1, 2).Symbol != ' ') {
+                //if middle row is full with same symbol
+                if (_board.TileAt(1, 0).Symbol ==
+                    _board.TileAt(1, 1).Symbol &&
+                    _board.TileAt(1, 2).Symbol ==
+                    _board.TileAt(1, 1).Symbol) {
+                    return _board.TileAt(1, 0).Symbol;
                 }
+            }
+
+            //if the positions in first row are taken
+            if (_board.TileAt(2, 0).Symbol != ' ' &&
+                _board.TileAt(2, 1).Symbol != ' ' &&
+                _board.TileAt(2, 2).Symbol != ' ') {
+                //if middle row is full with same symbol
+                if (_board.TileAt(2, 0).Symbol ==
+                    _board.TileAt(2, 1).Symbol &&
+                    _board.TileAt(2, 2).Symbol ==
+                    _board.TileAt(2, 1).Symbol) {
+                    return _board.TileAt(2, 0).Symbol;
+                }
+            }
 
             return ' ';
         }
